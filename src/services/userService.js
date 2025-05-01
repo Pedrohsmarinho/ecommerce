72cbb68b6005f0b16accc479e0c4309dd9dcb0a7
Pedro Marinho <pedrohsmarinho@outlook.com>
2025-05-01 04:18:41 -0300

    fix: replace bcryptjs with bcrypt for password hashing

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,5 @@
-const bcrypt = require('bcryptjs');
+const UserRepository = require('../repositories/userRepository');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const prisma = require('../config/database');
